fbd8d5216fe2b7400dd7be3087db1e4c37d17179
aasif-iqbal <aasif.iqbal3000@gmail.com>
2024-03-30 01:57:35 +0530

    all files added

---
+++ controllers/paymentController.js
@@ -0,0 +1,63 @@
+const Razorpay = require('razorpay'); 
+const { RAZORPAY_ID_KEY, RAZORPAY_SECRET_KEY } = process.env;
+
+const razorpayInstance = new Razorpay({
+    key_id: RAZORPAY_ID_KEY,
+    key_secret: RAZORPAY_SECRET_KEY
+});
+
+const renderProductPage = async(req,res)=>{
+
+    try {
+        //console.log('product log');
+        res.render('product');
+
+    } catch (error) {
+        console.log(error.message);
+    }
+
+}
+
+const createOrder = async(req,res)=>{
+    try {
+        const amount = req.body.amount*100
+        const options = {
+            amount: amount,
+            currency: 'INR',
+            receipt: 'razorUser@gmail.com'
+        }
+        
+        console.log('here', options);
+        
+        razorpayInstance.orders.create(options, 
+            (err, order)=>{
+                if(!err){
+                    res.status(200).send({
+                        success:true,
+                        msg:'Order Created',
+                        order_id:order.id,
+                        amount:amount,
+                        key_id:RAZORPAY_ID_KEY,
+                        product_name:req.body.name,
+                        description:req.body.description,
+                        contact:"9090901111",
+                        name: "aasif",
+                        email: "aasif@gmail.com"
+                    });
+                }
+                else{
+                    res.status(400).send({success:false,msg:'Something went wrong!'});
+                }
+            }
+        );
+
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+
+module.exports = {
+    renderProductPage,
+    createOrder
+}
